8f881024e4b38eff663c455b3d1ef67fab041b85
duccanhole <duccanhole@gmail.com>
2022-11-16 22:23:02 +0700

    add link document

--- algorithm/bfs.js
+++ algorithm/bfs.js
@@ -1,5 +1,7 @@
 const { Graph } = require('./graph.js');
 
+// document reference: https://www.tutorialspoint.com/Breadth-first-search-traversal-in-Javascript
+
 // theoraphy
 // - step 1: Visit the adjacent unvisited vertex. Mark it as visited. Display it. Insert it in a queue.
 // - step 2: If no adjacent vertex is found, remove the first vertex from the queue.
